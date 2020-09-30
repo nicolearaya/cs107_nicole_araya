fc8fe13486c159bebabf6813cdc6a51e20407832
Nicole Araya <nicolearaya0924@gmail.com>
2020-09-29 22:59:08 -0400

    submitting HW2 problems 2-4

---
+++ homework/HW2/HW2-final/P3b.py
@@ -0,0 +1,16 @@
+def outer(balance):
+    def withdrawal(w):
+        if w > balance:
+            return
+        else:
+            balance = balance - w
+            return balance
+    return withdrawal
+
+init_balance = 500
+wd = outer(init_balance)
+withdrawal_amount = 50
+new_withdrawal_amount = 100
+print(wd(withdrawal_amount))
+print(wd(new_withdrawal_amount))
+print("this does not work because the inner fuction does not have direct access to manipulating state of the outer function (balance). Names in the inner function (local scope) can not interfere with names in the global scope.")
