e99d9e7bdbfd85f63807330afef9021ca225090d
Nicole Araya <nicolearaya0924@gmail.com>
2020-10-05 17:32:50 -0400

    updated exercise

--- pair_programming/PP4/exercise_1.py
+++ pair_programming/PP4/exercise_1.py
@@ -1,19 +1,32 @@
 import numpy as np
 
 def layer(shape, actv):
-    #Check that shape is 2
-	assert len(shape) == 2
-    
-    #Add closure
-	def layer_closure(inputs, weights, bias):
-		print(f'Layer shape {shape}')
-		print(f'Shape of weights {weights.shape}')
-		print(f'Shape of input {inputs.shape}')
-		print(f'Shape of bias {bias.shape}')
 
-		assert weights.shape == shape, f'{weights.shape} vs {shape}'
-		assert bias.shape[0] == shape[1], f'{bias.shape} vs {shape[1]}'
+    #check that shape is 2
+    assert len(shape) == 2
 
-		return actv(np.matmul(inputs,weights) + bias)   
-        #actv(np.matmul(np.transpose(weights), np.transpose(inputs)) + bias)   
-	return layer_closure
+    def h_gen(inputs, weights, bias):
+        nonlocal shape
+        nonlocal actv
+
+        layer_outputs = np.zeros(shape[1])
+
+        for i in range(shape[1]):
+            layer_outputs[i] = actv(np.dot(weights, inputs) + bias[i])
+
+        return layer_outputs
+
+    return h_gen
+
+t = np.random.uniform(0, 1, 100).reshape(-1, 1)
+shape1 = np.array([len(t), 4])
+shape2 = np.array([len(t), shape1[1]])
+
+layer1 = layer(shape1, np.tanh)
+layer2 = layer(shape2, np.tanh)
+
+b1 = np.array([1, 2, 3, 4])
+b2 = np.random.uniform(0, 1, shape1[1])
+
+w1 = np.random.normal(0, 1, len(t))
+w2 = np.random.normal(0, 2, shape1[1])
