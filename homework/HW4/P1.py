7a423e56884ac7753b17f73e080d9aad0e7213dd
Nicole Araya <nicolearaya0924@gmail.com>
2020-10-27 16:41:02 -0400

    updated P1 answer

--- homework/HW4/P1.py
+++ homework/HW4/P1.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 xValues = []
 yValues = []
 
-hValues = [1*10**-1, 1*10**-7, 1*10**-15]
+hValues = [1*10**-1, 1*10**-7]
 
 #1*10**-15
-#this was taking too long to calculate, so I wasn't able to include it in my chart
+#this was taking too long to calculate, and eventually kept automatically killing the process so I wasn't able to include it in my chart
 
 def f(x):
     return np.log(x)
@@ -21,14 +21,8 @@ def numerical_diff(f,h):
         return function
     return calcDiff
 
-closure = numerical_diff(f, hValues[1])
-print(closure(0.2))
-print(closure(0.4))
-
 for i in hValues:
     a = 0.2
-    index = hValues.index(i)
-    print(index)
     xlist = []
     ylist = []
     closure = numerical_diff(f, i)
@@ -39,14 +33,16 @@ for i in hValues:
     xValues.append(xlist)
     yValues.append(ylist)
 
-
 exact_deriv = 1/np.array(xValues[1])
 
 pl1 = plt.plot(xValues[0], yValues[0], 'g', label='h=1*10**-1')
 pl2 = plt.plot(xValues[1], yValues[1], 'r', label = 'h=1*10**-7')
-pl3 = plt.plot(xValues[2], yValues[2], 'y--', label = 'h=1*10**-15')
+#pl3 = plt.plot(xValues[2], yValues[2], 'y--', label = 'h=1*10**-15')
 pl4 = plt.plot(xValues[1], exact_deriv, 'b:', label = 'Exact Derivative')
 plt.legend()
+
+print("Answer to Q-a: the value of h that was closest to the true derivative was 1*10**-7. When h is too small, the approximation takes too long compute. When h is too large, the linear approximation is not accurate.")
+print("Answer to Q-b: Automatic differentiation addresses the problems of heavy computation and inaccuracy, by breaking functions down into small elementary functions that are easy to compute yet still yield machine precision answers.")
 
 plt.xlabel('X Value')
 plt.ylabel('Finite Difference Approximation')
